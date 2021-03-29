6286dfba0de0fadef181bbc41b3bf75f2c82593a
Michael Burton <mail4mdb@gmail.com>
2021-03-29 15:39:25 -0500

    add app router to main server index file

--- server/index.js
+++ server/index.js
@@ -1,42 +1,11 @@
 const express = require("express");
 const { User } = require("../models/user");
-const { router } = require("../models/user");
+const router = require("./router");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
-app.post("/user", async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    console.log("new user", { newUser });
-
-    await newUser.save();
-
-    res.json({ user: newUser }); // Returns the new user that is created in the database
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-app.get("/user/:userId", async (req, res) => {
-  const userId = req.params.userId;
-
-  try {
-    const user = await User.findAll({
-      where: {
-        id: userId,
-      },
-    });
-
-    res.json({ user });
-  } catch (error) {
-    console.error(error);
-  }
-});
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT: ${PORT}`);
--- server/router.js
+++ server/router.js
@@ -1,28 +1,33 @@
-app.post("/user", async (req, res) => {
+const express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+router.post("/user", async (req, res) => {
   try {
     const newUser = new User(req.body);
     console.log("new user", { newUser });
-
     await newUser.save();
-
     res.json({ user: newUser }); // Returns the new user that is created in the database
   } catch (error) {
     console.error(error);
   }
 });
 
-app.get("/user/:userId", async (req, res) => {
+router.get("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
-
   try {
     const user = await User.findAll({
       where: {
         id: userId,
       },
     });
-
     res.json({ user });
   } catch (error) {
     console.error(error);
   }
 });
+
+module.exports = router;
