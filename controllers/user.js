8ad9a5d774661e3d77b385ef49096664c2fa2eec
Michael Burton <mail4mdb@gmail.com>
2021-03-30 12:16:58 -0500

    externalize route logic into controllers folder

---
+++ controllers/user.js
@@ -0,0 +1,51 @@
+const client = require("../db");
+const User = require("../models/user");
+
+const addUser = async (req, res) => {
+  try {
+    const { query } = req;
+    const user = new User(query);
+    await user.save();
+    res.json({ user }); // Returns the new user that is created in the database
+  } catch (error) {
+    console.error(error);
+    const formattedError = `Unable to create new user\n 
+      incoming query: ${query} \n 
+      error: ${error}`;
+    res.status(404).send(formattedError);
+  }
+};
+
+const getUser = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await User.findAll({
+      where: {
+        id: userId,
+      },
+    });
+    res.json({ user });
+  } catch (error) {
+    console.error(error);
+    const formattedError = `Unable to get new user\n 
+      incoming userId: ${userId} \n 
+      error: ${error}`;
+    res.status(404).send(formattedError);
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const query = "SELECT * FROM users";
+    const users = client.query(query);
+    console.log("retrieving users", { users });
+    res.json({ users }); // Returns the new user that is created in the database
+  } catch (error) {
+    console.error(error);
+    const formattedError = `Unable to select * users\n 
+      error: ${error}`;
+    res.status(404).send(formattedError);
+  }
+};
+
+module.exports = { addUser, getUser, getUsers };
